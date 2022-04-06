71546df39c17057fdc8aef3a87f245e453ad0991
HASSAN1A <okothhassanjuma@gmail.com>
2022-04-07 01:19:04 +0300

    get object

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,14 @@
+export default function getBudgetObject(income, gdp, capita) {
+  // const budget = {
+  //   income: income,
+  //   gdp: gdp,
+  //   capita: capita,
+  // };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
